6de88570cb976e378df639364e3122e764048af4
jneema <joeyshem33@gmail.com>
2025-02-11 16:21:29 +0300

    redux redo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import LandingPage from "./components/landing-page";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
